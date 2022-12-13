702ba966217493cae0b673c385ce06232f2528f5
Zachary Flegle <zflegle3@gmail.com>
2022-12-13 12:55:55 -0500

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://userMcUserface:password*123@cluster0.npybxka.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://userMcUserface:password*123@cluster0.npybxka.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
